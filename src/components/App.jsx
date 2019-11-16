88583365ab8d50214e32a4f502f970b4e25fc06b
Student <example@example.com>
2019-11-15 16:17:12 -0800

    remove commented out code from App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import BeerList from "./BeerList";
 import { Switch, Route } from "react-router-dom";
-// import ClassList from "./ClassList";
 import Home from "./Home";
 import NavBar from "./NavBar";
 import Error404 from "./Error404";
